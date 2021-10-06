36005a81d782f1cc3c2cc421e3d1dc80643c585c
Santiago Sacristan <sacristan.santiago@gmail.com>
2021-10-05 22:42:53 -0300

    se agrega persistencia session en mongo atlas

--- src/services/server.ts
+++ src/services/server.ts
@@ -4,8 +4,29 @@ import path from "path";
 import * as http from "http";
 import apiRouter from "../routes/index";
 import handlebars from "express-handlebars";
+import cookieParser from "cookie-parser";
+import MongoStore from "connect-mongo";
+import Config from "../config"
+
+const usuario = Config.MONGO_ATLAS_USER;
+const password = Config.MONGO_ATLAS_PASSWORD;
+const dbName = Config.MONGO_ATLAS_DBNAME;
+const clusterUrl = Config.MONGO_ATLAS_CLUSTER
+const myURI = `mongodb+srv://${usuario}:${password}@${clusterUrl}/${dbName}?retryWrites=true&w=majority`
+
+const  oneMin = 1000 * 60; 
+const StoreOptions = {
+  store: MongoStore.create({
+    mongoUrl: myURI,
+  }),
+  secret: 'thisismysecrctekeyfhrgfgrfrty84fwir767',
+  saveUninitialized: true,
+  cookie: { maxAge: oneMin },
+  resave: true,
+}
 
 const app = express();
+app.use(cookieParser())
 
 const publicFolder = path.resolve(__dirname, "../../public");
 app.use(express.static(publicFolder));
@@ -13,15 +34,7 @@ app.use(express.static(publicFolder));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-const  oneMin = 1000 * 60; 
-app.use(
-  session({
-    secret: 'thisismysecrctekeyfhrgfgrfrty84fwir767',
-    saveUninitialized: true,
-    cookie: { maxAge: oneMin },
-    resave: true,
-  })
-);
+app.use(session(StoreOptions));
 
 app.use("/api", apiRouter);
 
